a5e38bb9e30257200dd54855b0a6ce2c5016a129
Charlie J A <ca15005@my.bristol.ac.uk>
2019-02-11 18:03:35 +0000

    $string

--- gitsync.sh
+++ gitsync.sh
@@ -1,4 +1,8 @@
 
+# prepare date-time string
+
+time=$(date +"%a %d-%b%Y %T")
+string=${time^^}
 
 # list of files to add, combine with .gitignore
 
@@ -9,7 +13,7 @@ git add ./ernie
 
 # rest of the sync commands
 
-git commit -m 'auto sync'
+git commit -m '$string'
 #git ls-tree -r master --name-only
 git push -u origin master
 
